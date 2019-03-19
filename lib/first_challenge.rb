51438e9d70495a368782dfaaac34b67e8a282b78
Andrew Waller <andrewhwaller@gmail.com>
2019-03-19 01:21:26 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
 contacts["Freddy Mercury"].each do |attribute, value|
-    if attribute == [:favorite_icecream_flavors]
+    if attribute == :favorite_icecream_flavors
       value.delete_if == "strawberry"
   end
 end
